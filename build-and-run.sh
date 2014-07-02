e73509a10e5cf913a77fc491daaca305de55425a
Jeff Moody <fifthecho@gmail.com>
2014-07-02 14:21:52 -0400

    fixing issues with a fresh os install

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ansible-playbook ./build-docker-container.yml && sleep 15 \
-&& ansible-playbook ./configure-environment.yml && sleep 5 \
+ansible-playbook -i ./local-hosts ./build-docker-container.yml && sleep 15 \
+&& ansible-playbook -i ./local-hosts ./configure-environment.yml && sleep 5 \
 && ansible-playbook -i ./hosts ./setup-cloudstack.yml
