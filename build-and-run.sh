4ed776bb37affd8722657acbdb79eb3a8e39757b
Jeff Moody <fifthecho@gmail.com>
2014-07-02 17:53:05 -0400

    Fix incorrect handling of Archlinux and correctly handling hostkey checking on the playbook against the container.

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ansible-playbook -i ./local-hosts ./build-docker-container.yml && sleep 15 \
 && ansible-playbook -i ./local-hosts ./configure-environment.yml && sleep 5 \
-&& ansible-playbook -i ./hosts ./setup-cloudstack.yml
+&& ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./hosts ./setup-cloudstack.yml
